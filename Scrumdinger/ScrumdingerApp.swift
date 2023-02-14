c429ddbd22f572cb22aea23fd8446968c1644522
Rodrigo Tristany <rodrigo@snowshoestamp.com>
2023-02-14 16:09:16 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Rodrigo Tristany on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
