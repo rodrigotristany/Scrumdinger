6bcc5c9326920af261d81579ce5908e67942228c
Rodrigo Tristany <rodrigo@snowshoestamp.com>
2023-02-14 18:12:51 -0300

    Lesson: Displaying data in a list

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -5,4 +5,20 @@
 //  Created by Rodrigo Tristany on 14/02/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums) {
+            CardView(scrum: $0)
+                .listRowBackground($0.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
